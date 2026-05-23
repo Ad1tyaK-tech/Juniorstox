87530d7e48a503e965bb39d32f2e5a37bfee9406
Aditya Kiran <adityakiran0123@gmail.com>
2026-05-23 13:45:39 -0700

    Initial Commit

---
+++ stox juniorTests/stox_juniorTests.swift
@@ -0,0 +1,18 @@
+//
+//  stox_juniorTests.swift
+//  stox juniorTests
+//
+//  Created by Aditya Kiran on 5/23/26.
+//
+
+import Testing
+
+struct stox_juniorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
